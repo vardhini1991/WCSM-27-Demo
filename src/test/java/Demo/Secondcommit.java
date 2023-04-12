ff182c6810ccff070022607110bbd8466aa9c2da
vardhini <vardhininaga108@gmail.com>
2023-04-12 15:35:18 +0530

    2nd package commit n push

---
+++ src/test/java/Demo/Secondcommit.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Secondcommit 
+{
+  @Test
+  public void scommit()
+  {
+	  System.out.println("second package");
+  }
+  
+  
+  
+}
